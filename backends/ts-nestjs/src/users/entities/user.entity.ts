01e449df15a2e2495210eafeaedad9b51e8cfef5
Seong-Yun Min <97732161+lessquo@users.noreply.github.com>
2026-05-30 18:28:01 +0900

    Define User entity

--- backends/ts-nestjs/src/users/entities/user.entity.ts
+++ backends/ts-nestjs/src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn({ type: 'uuid', default: () => 'uuidv7()' })
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+}
