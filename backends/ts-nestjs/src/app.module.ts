31ccbdc30f7237275f23e6ac04c7623b8a9dd7f4
Seong-Yun Min <97732161+lessquo@users.noreply.github.com>
2026-05-30 16:48:31 +0900

    Add Postgres service and TypeORM connection

--- backends/ts-nestjs/src/app.module.ts
+++ backends/ts-nestjs/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
