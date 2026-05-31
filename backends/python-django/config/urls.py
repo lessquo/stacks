a1b93dc261246475c715d23df54d2c70b5565dee
Seong-Yun Min <97732161+lessquo@users.noreply.github.com>
2026-06-01 02:58:44 +0900

    Add health check route

--- backends/python-django/config/urls.py
+++ backends/python-django/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from config.views import health
+
 urlpatterns = [
+    path('', health),
     path('admin/', admin.site.urls),
     path('', include('apps.users.urls')),
 ]
