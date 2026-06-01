07b6b1ad4e4424d9d8e720c825337edffd4038d4
Seong-Yun Min <97732161+lessquo@users.noreply.github.com>
2026-06-01 15:14:29 +0900

    Return 405 for non-GET on health route

--- backends/python-django/config/views.py
+++ backends/python-django/config/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_GET
 
 
+@csrf_exempt
+@require_GET
 def health(request):
     return HttpResponse(status=200)
